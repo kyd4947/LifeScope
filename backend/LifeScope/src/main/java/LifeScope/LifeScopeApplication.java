57b4a4acd5f1d9207986f536aea12d2768068bbf
kyd4947 <kyd4947@gmail.com>
2026-08-25 21:40:03 +0900

    feat : backend 구조

---
+++ backend/LifeScope/src/main/java/LifeScope/LifeScopeApplication.java
@@ -0,0 +1,13 @@
+package LifeScope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeScopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeScopeApplication.class, args);
+	}
+
+}
---
+++ backend/LifeScope/src/test/java/LifeScope/LifeScopeApplicationTests.java
@@ -0,0 +1,13 @@
+package LifeScope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeScopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
