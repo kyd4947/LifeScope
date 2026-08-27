35daac77f9d7e81468b73160ab7c8f24b2ae4981
kyd4947 <kyd4947@gmail.com>
2026-08-27 22:00:50 +0900

    feat : city entity 구현

---
+++ backend/LifeScope/src/main/java/com/lifescope/domain/city/City.java
@@ -0,0 +1,58 @@
+package com.lifescope.domain.city;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="city")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class City {
+
+	@Id
+	@Column(length=10)
+	private String code;
+	
+	@Column(nullable=false, length=50)
+	private String name;
+	
+	@Column(length=10)
+	private String parentCode;
+	
+	@Column(nullable=false)
+	private Short level;
+	
+	private Double latitude;
+	private Double longitude;
+	
+	@Builder.Default
+	@Column(nullable=false)
+	private Boolean isActivce = true;
+	
+	@OneToMany(mappedBy="city", cascade=CascadeType.ALL, orphanRemoval=true)
+	@Builder.Default
+	private List<ConsumerPriceIndex> cpiList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "city", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<AverageWage> wageList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "city", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<HousingPrice> housingList = new ArrayList<>();
+}
