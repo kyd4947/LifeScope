73e15c24b4d56499fe0c8306ace409be37bdb410
kyd4947 <kyd4947@gmail.com>
2026-08-27 21:04:23 +0900

    feat : 의존성 추가

---
+++ backend/LifeScope/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "lifescope-backend"
---
+++ backend/LifeScope/src/test/java/com/lifescope/lifescope_backend/LifescopeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifescope.lifescope_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifescopeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
