f82ee26addb3b516a96eeeff633c2ec95c16fc1c
kyd4947 <kyd4947@gmail.com>
2026-09-04 17:05:15 +0900

    update : 잘못된 거래 유형 클래스명 수정

--- backend/LifeScope/src/main/java/com/lifescope/exception/InvalidTradeTypeException.java
+++ backend/LifeScope/src/main/java/com/lifescope/exception/InvalidTradeTypeException.java
@@ -1,9 +1,9 @@
 package com.lifescope.exception;
 
 // 유효하지 않은 거래 유형 (매매 / 전세 / 월세 외) 요청됐을 때 발생하는 예외 - GlobalExceptionHandler 에서 400 Bad Request 로 변환
-public class InvalideTradeTypeException extends RuntimeException{
+public class InvalidTradeTypeException extends RuntimeException{
 
-	public InvalideTradeTypeException(String message) {
+	public InvalidTradeTypeException(String message) {
 		super(message);
 	}
 }
