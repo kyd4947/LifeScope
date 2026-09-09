b021c10c683ae4c99c236e853b80111e0de35a22
kyd4947 <kyd4947@gmail.com>
2026-09-09 20:04:13 +0900

    feat : 의존성 추가

--- backend/LifeScope/build.gradle.kts
+++ backend/LifeScope/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-flyway")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	implementation("org.springframework.boot:spring-boot-starter-restclient")
 	implementation("org.flywaydb:flyway-database-postgresql")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.1.0")
 	compileOnly("org.projectlombok:lombok")
