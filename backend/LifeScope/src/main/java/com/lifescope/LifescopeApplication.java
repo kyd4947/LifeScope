fdfa4d461b87640df4c0ecd0b3f6dd79f97fe003
kyd4947 <kyd4947@gmail.com>
2026-09-12 20:26:05 +0900

    feat : 스케줄 활성화 어노테이션 추가

--- backend/LifeScope/src/main/java/com/lifescope/LifescopeApplication.java
+++ backend/LifeScope/src/main/java/com/lifescope/LifescopeApplication.java
@@ -2,8 +2,10 @@ package com.lifescope;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling					// 월간 데이터 동기화 @Scheduled 활성화
 public class LifescopeApplication {
 
 	public static void main(String[] args) {
